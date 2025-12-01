2e6ac278b551d97d1c89569b22a0f97b5d712cb8
suhailrahmane-ctrl <suhailrahmane@gmail.com>
2025-12-01 16:41:32 +0430

    Update model_config.py

--- model_config.py
+++ model_config.py
@@ -1,1 +1,11 @@
+import os
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
 MODEL_NAME = "deepseek/deepseek-chat"
+
+HEADERS = {
+    "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+    "HTTP-Referer": "https://afg-thebest-omniai.streamlit.app/",
+    "X-Title": "AFG Thebest OmniAI"
+}
