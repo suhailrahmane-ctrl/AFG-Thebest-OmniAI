5c2ed7519285398b889541393c8febe4f1af1745
suhailrahmane-ctrl <suhailrahmane@gmail.com>
2025-12-01 21:29:40 +0430

    Update model_config.py

--- model_config.py
+++ model_config.py
@@ -1,5 +1,7 @@
-DEEPSEEK_API_KEY = "sk-9a6ae06e90e44207a13c6d8eedc4d393"  # --  ds-xxxxxx کلیدت را بذار
-MODEL_NAME = "deepseek/deepseek-chat"  # مدل چت DeepSeek
+# model_config.py
+DEEPSEEK_API_KEY = "sk-9a6ae06e90e44207a13c6d8eedc4d393"  # کلید مستقیم
+
+MODEL_NAME = "deepseek/deepseek-chat"
 
 HEADERS = {
     "Authorization": f"Bearer {DEEPSEEK_API_KEY}"
